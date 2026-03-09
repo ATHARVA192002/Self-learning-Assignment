6e7a736c144a004d576028d3b38e36aa785293bd
ATHARVA SHEJAL <96768018+ATHARVA192002@users.noreply.github.com>
2026-03-09 14:58:42 +0530

    Update Q3.cpp

--- Cpp/Q3.cpp
+++ Cpp/Q3.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-void swap(int* n1, int* n2){
-	int* temp;
-	temp=n1;
-	n1=n2;
-	n2=temp;
+void swap1(int* n1, int* n2){
+	int temp;
+	temp=*n1;
+	*n1=*n2;
+	*n2=temp;
 }
 
 int main(){
@@ -14,9 +14,7 @@ int main(){
 	cin>>num1>>num2;
 	
 	cout<<"Before swapping: "<<num1<<" "<<num2<<endl;
-	swap(num1,num2);
+	swap1(&num1,&num2);
 	cout<<"After swapping: "<<num1<<" "<<num2;
 	return 0;
 }
-
-

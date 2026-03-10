9c820b8bb92f865115e495f7940af13ed3372bff
ATHARVA SHEJAL <96768018+ATHARVA192002@users.noreply.github.com>
2026-03-10 13:28:01 +0530

    Add files via upload

---
+++ WPT_/Assingment_10_03_26/shejal.js
@@ -0,0 +1,16 @@
+let b = 
+{
+	id : 100,
+	name : "Shejal",
+	email : "shejal@gmail.com"
+	}
+	
+	for(const show in b)
+	{
+		if(Object.hasOwn(b,show))
+		{
+			const element = b[key];
+			console.log(element);
+		}
+	}
+			
